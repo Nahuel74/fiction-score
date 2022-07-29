ffb25e1374ef121e95b5191c798549ce47e089a4
Nahuel74 <Nahuelmdq74@gmail.com>
2022-07-29 13:51:12 -0300

    Cambios en el título
    Función que cambia el título principal, según la categoría seleccionada

---
+++ change_title.js
@@ -0,0 +1,6 @@
+const mainTitle = document.getElementById("main-title")
+const selectCategory = document.getElementById("select-category")
+
+selectCategory.onchange = function(){
+	mainTitle.innerHTML = "RATE THE " + selectCategory[selectCategory.selectedIndex].innerHTML.toUpperCase()
+}
