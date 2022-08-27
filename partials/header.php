6f8cf96674193bba8b665d4598d8103684b70b54
Nahuel74 <Nahuelmdq74@gmail.com>
2022-08-26 23:14:14 -0300

    Delete header.php

--- partials/header.php
+++
@@ -1,7 +0,0 @@
-<header id="topbar">
-    <nav>
-        <a href="/index.php">NEW</a>
-        <a href="/links/list.php">LIST</a>
-        <a href="/links/login.php">LOG IN</a>
-    </nav>
-</header>
